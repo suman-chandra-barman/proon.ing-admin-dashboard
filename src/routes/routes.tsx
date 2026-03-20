d3aa3412008a33e362886191f9c1d33f67db268c
Suman Chandra Barman <sumanchandrabarman999@gmail.com>
2026-03-20 11:42:55 +0600

    feat: update routing structure and improve CSS organization

---
+++ src/routes/routes.tsx
@@ -0,0 +1,11 @@
+import App from "@/App";
+import { createBrowserRouter } from "react-router";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
+export default router;
